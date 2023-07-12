e887bf4c4d85c1f64d5087adacbe754304b035ba
sangtt18 <SangTt18@fpt.com.vn>
2023-07-12 20:50:23 +0700

    Initial Commit

---
+++ SwiftUI_Study/SwiftUI_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StudyApp.swift
+//  SwiftUI_Study
+//
+//  Created by Sang Truong on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
